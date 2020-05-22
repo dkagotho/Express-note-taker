d434cb3ab209e13b2d18f889de4554e7e306d549
dkagotho <dianakago2@gmail.com>
2020-05-22 18:15:15 -0400

    made edits in the routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,17 +16,18 @@ module.exports = function(app) {
       text: req.body.text
     };
     
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
       if (err) throw err;
 
       const allNotes = JSON.parse(data);
 
       allNotes.push(newNote);
 
-      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
+      fs.writeFile("db/db.json", JSON.stringify(allNotes, null, 2), err => {
         if (err) throw err;
         res.send(db);
         console.log("Note created!")
+        console.log(allNotes)
       });
     });
   });
@@ -35,13 +36,13 @@ module.exports = function(app) {
 
     let noteId = req.params.id;
 
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
       if (err) throw err;
 
       const allNotes = JSON.parse(data);
       const newAllNotes = allNotes.filter(note => note.id != noteId);
 
-      fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
+      fs.writeFile("db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
         if (err) throw err;
         res.send(db);
         console.log("Note deleted!")
